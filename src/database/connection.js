98bcd7bf0400f45539a4badba30faaad54c65069
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-02 23:29:45 +0000

    😀️ Feat : correcao para poder criar models com npx sequelize-cli model:generate

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
-const {dbConfig} = require('../config/database.config');
-const connection = new Sequelize(dbConfig.development);
+const allConfigs = require('../config/database.config');
+const config = allConfigs[process.env.NODE_ENV];
+const connection = new Sequelize(config);
 
 module.exports = connection;
