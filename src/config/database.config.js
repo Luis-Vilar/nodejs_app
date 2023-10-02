6f4541bdce24a1749685395b31423044c75f9915
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-02 22:02:35 +0000

    Inicializaçao e correcao da conexao do sequelize

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,11 +1,35 @@
 const { config } = require("dotenv");
 config();
+const dbConfig = 
+{
+  "development": {
+    "username":  process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
+
+  },
+  "test": {
+    "username":  process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "database_test",
+    "host": process.env.DB_HOST,
+    'dialect': process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
+  },
+  "production": {
+    "username":  process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": "database_production",
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
+  }
+}
+
 
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-};
+  dbConfig
+}
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/database.config');
-const connection = new Sequelize(dbConfig);
+const {dbConfig} = require('../config/database.config');
+const connection = new Sequelize(dbConfig.development);
 
 module.exports = connection;
